232971de04dcc104380efc9fbeba829da9983414
icy-s <evgeny.tailov@gmail.com>
2025-01-27 11:25:34 +0200

    Add project files.

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,18 @@
+
+from datetime import *
+tana=date.today()
+print(f"Tere! Täna on {tana}")
+
+# 27/12/2022
+tana = tana.strftime("%d/%m/%Y")
+print(f"Tere! Täna on {tana}")
+
+# # December 27, 2022
+# tana = tana.strftime("%B %d, %Y")
+# print(f"Tere! Täna on {tana}")
+
+# # 12/27/22
+# tana = tana.strftime("%m/%d/%y")
+
+# # Dec-27-2022
+# tana = taba.strftime("%b-%d-%Y")
---
+++ PythonApplication2/module1.py
@@ -0,0 +1,14 @@
+from datetime import *
+from calendar import *
+tana=date.today()
+print(f"Tere! T‰na on {tana}")
+
+# 27/12/2022
+tana_ = tana.strftime("%d/%m/%Y")
+print(f"Tere! T‰na on {tana_}")
+paevadekogus=monthrange(2025,1)[1]
+print(f"Jaanuaris on {paevadekogus} p‰eva")
+p‰evad=tana.day
+onj‰‰nud=paevadekogus-p‰evadprint(f"Jaanuaris on j‰‰nud {onj‰‰nud} p‰eva")
+aastalıpuni
+print(f"Aasta lıpuni on j‰‰nud {}")
